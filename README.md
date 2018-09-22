c8d6c714f18f35d0f1d723462580458f48d13d41
notthebee <mislead@protonmail.com>
2018-09-22 13:49:02 +0200

    added -n and -w arguments, made -u a required argument, changed URL handling

--- README.md
+++ README.md
@@ -2,20 +2,6 @@
 A small script that scrapes a webpage and displays a graph of the most used words  
 ![Screenshot](screenshot.png)  
 
-usage: `pageparse.py [-h] -u URL [-l LANG] [-e EXCLUDE]`  
-
-### optional arguments:  
-* `-h, --help`  
-show this help message and exit  
-* `-l LANG, --lang LANG`  
-Language of the webpage (english, french, russian, spanish, german, italian, portuguese). Requires optional dependencies to be installed. Defaults to English  
-* `-e EXCLUDE, --exclude EXCLUDE`  
-Exclude words from the graph, divided by spaces. For example: "lorem ipsum dolor sit amet"  
-
-### required arguments:
-* `-u URL, --url URL`  
-URL of the page, for example: https://en.wikipedia.org/wiki/Python_(programming_language)  
-
 ### dependencies:
 * BeautifulSoup
 * urllib
--- pageparse.py
+++ pageparse.py
@@ -24,31 +24,37 @@ class color:
 
 # Arguments
 ap = argparse.ArgumentParser(description=color.GREEN + 'A small script that scrapes a webpage and displays a graph of the most used words' + color.END)
-req = ap.add_argument_group('required arguments')
-req.add_argument("-u", "--url", 
-                required=True, 
-                help="URL of the page, for example: https://en.wikipedia.org/wiki/Python_(programming_language)")
+ap.add_argument("url", 
+                help="URL of the page, for example: https://en.wikipedia.org/)" or "fsf.org")
 ap.add_argument("-l", "--lang", 
                 required=False, 
                 default="english", 
                 help="Language of the webpage (english, french, russian, spanish, german, italian, portuguese). Requires optional dependencies to be installed. Defaults to English")
+ap.add_argument("-w", "--wordnum",
+				required=False,
+				type=int,
+				default=20,
+				help="Number of words in the graph. Defaults to 20")
+ap.add_argument("-n", "--nospellcheck",
+                required=False,
+                action='store_true',
+                help="Turn off spellchecking")
 ap.add_argument("-e", "--exclude", 
                 required=False, 
                 help='Exclude words from the graph, divided by spaces. For example: "lorem ipsum dolor sit amet"')
 args=vars(ap.parse_args())
 
-# Checking if the URL is valid
-if not args["url"].startswith("http"):
-    print("ERROR: Invalid URL (don't forget 'http://' or 'https://')")
-    sys.exit(1)
+# Appending http:// to the URL in case not specified by the user
 url = args["url"]
+if not url.startswith("http"):
+	url = "http://" + url
 
 # Checking if the language is valid
+lang = args["lang"]
 valid_languages=("english", "french", "russian", "spanish", "german", "italian", "portuguese")
 if not args["lang"] in valid_languages:
     print("ERROR: Invalid language. Supported options: english, french, russian, spanish, german, italian, portuguese")
     sys.exit(1)
-lang = args["lang"]
 
 # Scraping the page
 req = urllib.request.Request(url, headers={'User-Agent' : "Magic Browser"})
@@ -71,20 +77,26 @@ for word in words:
     if not word in sr:
         clean_words.append(word)
 
-# Removing "non-words"
-if lang in ['english', 'french', 'italian', 'russian']:
-    langshort = lang[:2]
-elif lang == 'german':
-    langshort = 'de'
-elif lang == 'spanish':
-    langshort = 'es'
-elif lang == 'portuguese':
-    langshort == 'pt'
-d = enchant.Dict(langshort)
-correct_words = []
-for word in clean_words:
-	if d.check(word):
-		correct_words.append(word)
+# Removing "non-words" and spellchecking unless turned off by user
+def spellCheck():
+    if lang in ['english', 'french', 'italian', 'russian']:
+     langshort = lang[:2]
+    elif lang == 'german':
+        langshort = 'de'
+    elif lang == 'spanish':
+        langshort = 'es'
+    elif lang == 'portuguese':
+        langshort == 'pt'
+    d = enchant.Dict(langshort)
+    correct_words = []
+    for word in clean_words:
+        if d.check(word):
+            correct_words.append(word)
+    return correct_words
+if args["nospellcheck"] == False:
+    correct_words = spellCheck()
+else:
+    correct_words = clean_words
 # Also excluding the words if specified by the user
 if args["exclude"]:
 	exclude = args["exclude"].split()
@@ -94,7 +106,7 @@ else:
 
 # Displaying the plot
 freq = nltk.FreqDist(words).copy()
-freq.plot(20, 
+freq.plot(args["wordnum"], 
           cumulative=False, 
           title='Frequency plot for ' + url, 
           linewidth=2,
